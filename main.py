74d2675550f5647e4504ea355be972d86c7ff72c
lizstangle <lizstangle@me.com>
2020-09-22 22:22:50 -0700

    elif edit

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ def drink_preference(drink, time):
 
     if drink == "coffee":
         if time == "morning":
-            return "How about a cappuccino? You can have whatever coffee drink you like, with or without dairy." 
+            print("How about a cappuccino? You can have whatever coffee drink you like, with or without dairy.") 
 
         elif time == "afternoon":
-            return "Have an espresso, americano or drip coffee and hold the milk!"
-        elif time == "night":
-            return "Skip the coffee and have some water."
-        else:
-            return "Water is always a great option."
+            print("Have an espresso, americano or drip coffee and hold the milk!")
+
+        else time == "night":
+            print("Skip the coffee and have some water.")
+            
+    if drink == "water":
+            print("Water is always a great option.")
         # return "Good morning! Feel free to enjoy any coffee you like, with or without milk. 
         #     You could have a cappuccino, a brew coffee with cream, a cafe au lait, a machiato. There are so many 
         #     delicious options!"
@@ -20,30 +22,30 @@ def drink_preference(drink, time):
 
     elif drink == "tea":
         if time == 'morning':
-            return "Enjoy a black tea or a green tea, with or without milk."
+            print("Enjoy a black tea or a green tea, with or without milk.")
 
         elif time == "afternoon":
-            return "Enjoy a green or herbal tea and hold the milk."
+            print("Enjoy a green or herbal tea and hold the milk.")
 
         elif time == "night":
-            return "Enjoy an herbal tea and hold the milk."
+            print("Enjoy an herbal tea and hold the milk.")
 
     elif drink == "water":
-        return "Water is always an excellent choice!"            
+        print("Water is always an excellent choice!")            
 
-valid_drinks = ["Coffee", "Tea", "Water"]
+valid_drinks = ["coffee", "tea", "water"]
 valid_drink = False
-while valid_drink == False:
-    drink = input("Would you like Coffee or Tea?")
+while valid_drink is False:
+    drink = input("Would you like coffee or tea?")
     if drink in valid_drinks:
         valid_drink = True
 
-valid_times = ["Morning", "Afternoon", "Night"]
+valid_times = ["morning", "afternoon", "night"]
 valid_time = False
-while valid_time == False:
+while valid_time is False:
     time = input("Will you be drinking this in the morning, afternoon or night?")
     if time in valid_times:
         valid_time = True
     
 rec = drink_preference(drink, time)
-print("definitely go for: "  + rec)
+# print("definitely go for: "  + rec)
